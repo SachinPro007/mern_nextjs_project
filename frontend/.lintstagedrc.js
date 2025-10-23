baec516b95edd8155f12c19c6345ee4914c43a4d
Sachin <mrsachinsehrawat07@gmail.com>
2025-10-23 10:27:32 +0530

    another husky testing

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => `"${path.relative(process.cwd(), f)}"`)
-    .join(" ")}`;
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`;
 
 const buildPrettierCommand = (filenames) =>
   `prettier --write ${filenames.join(" ")}`;
